0968f1f9124dd932bd98c77c520d1576ef9671a4
Mimerel <support@mimerel.fr>
2019-02-14 14:48:34 +0100

    Update datesTimes.go

--- datesTimes.go
+++ datesTimes.go
@@ -36,3 +36,36 @@ func TimestampFromYYYYMMDD(moment string) (date time.Time, err error) {
   layout := "2006-01-02T15:04:05.000Z"
   return time.ParseInLocation(layout, moment, time.Local)
 }
+
+func Timestamp2BeginningOfMonthUTC(moment time.Time) time.Time {
+	y, m, _ := moment.Date()
+	return time.Date(y, m, 1, 0, 0, 0, 0, time.UTC)
+}
+
+func Timestamp2BeginningOfDayUTC(moment time.Time) time.Time {
+	y, m, d := moment.Date()
+	return time.Date(y, m, d, 0, 0, 0, 0, time.UTC)
+}
+
+func Timestamp2BeginningOfYearUTC(moment time.Time) time.Time {
+	_, m, d := moment.Date()
+	return time.Date(1, m, d, 0, 0, 0, 0, time.UTC)
+}
+
+func Timestamp2EndOfMonthUTC(moment time.Time) time.Time {
+	return Timestamp2BeginningOfMonthUTC(moment).AddDate(0, 1, 0).Add(-time.Nanosecond)
+}
+
+func Timestamp2EndOfDayUTC(moment time.Time) time.Time {
+  return Timestamp2BeginningOfDayUTC(moment).AddDate(0, 0, 1).Add(-time.Nanosecond)
+}
+
+func Timestamp2EndOfYearUTC(moment time.Time) time.Time {
+  return Timestamp2BeginningOfYearUTC(moment).AddDate(1, 0, 0).Add(-time.Nanosecond)
+}
+
+func TimestampFromYYYYMMDDUTC(moment string) (date time.Time, err error) {
+  moment += "T12:00:00.000Z"
+  layout := "2006-01-02T15:04:05.000Z"
+  return time.ParseInLocation(layout, moment, time.UTC)
+}
