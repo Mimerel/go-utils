6d7e50f29f2122d8fc8c9741904704157a39a43b
Mimerel <support@mimerel.fr>
2019-03-14 15:04:13 +0100

    added file copy

--- files.go
+++ files.go
@@ -1,6 +1,7 @@
 package go_utils
 
 import (
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
@@ -39,3 +40,25 @@ func ScanDirectory(path string, extensions []string, ignore []string) (folders [
 	return folders, files, scanError
 }
 
+func CopyFileContents(src, dst string) (err error) {
+	in, err := os.Open(src)
+	if err != nil {
+		return
+	}
+	defer in.Close()
+	out, err := os.Create(dst)
+	if err != nil {
+		return
+	}
+	defer func() {
+		cerr := out.Close()
+		if err == nil {
+			err = cerr
+		}
+	}()
+	if _, err = io.Copy(out, in); err != nil {
+		return
+	}
+	err = out.Sync()
+	return
+}
