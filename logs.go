fae58c65517cb8651a7bd865c21be3c3c9d870be
Mimerel <support@mimerel.fr>
2019-05-02 08:39:37 +0200

    feat(logs): Added simple logger

--- logs.go
+++ logs.go
@@ -1,8 +1,48 @@
 package go_utils
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type LogLevel int
+
+const (
+	Debug LogLevel = 0
+	Info  LogLevel = 1
+	Error LogLevel = 2
+)
 
 func DefaultLogOutput(message string, args ...interface{}) {
 	fmt.Printf(message, args)
 }
 
+type LogParams struct {
+	level LogLevel
+}
+
+func New(level LogLevel) LogParams {
+	l := LogParams{level: level}
+	return l
+}
+
+func (l LogParams) Info(message string, args ...interface{}) {
+	if l.level >= 1 {
+		computedMessage := fmt.Sprintf(message, args...)
+		fmt.Printf(time.Now().Format(time.RFC3339)+" - Info : %s \n", computedMessage)
+	}
+}
+
+func (l LogParams) Debug(message string, args ...interface{}) {
+	if l.level >= 0 {
+		computedMessage := fmt.Sprintf(message, args...)
+		fmt.Printf(time.Now().Format(time.RFC3339)+" - Debug : %s \n", computedMessage)
+	}
+}
+
+func (l LogParams) Error(message string, args ...interface{}) {
+	if l.level >= 2 {
+		computedMessage := fmt.Sprintf(message, args...)
+		fmt.Printf(time.Now().Format(time.RFC3339)+" - Error : %s \n", computedMessage)
+	}
+}
