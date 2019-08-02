c3a4139cb51c7feb6caad78c2a01711297119caa
Steven LEGUIN <s.leguin@mimerel.fr>
2019-08-02 11:54:44 +0200

    fix(logs): color

--- logs.go
+++ logs.go
@@ -8,14 +8,14 @@ import (
 type LogLevel int
 
 const (
-	Debug LogLevel = 2
-	Info  LogLevel = 1
-	Error LogLevel = 0
-	InfoColor    = "\033[1;34m%s\033[0m"
+	Debug     LogLevel = 2
+	Info      LogLevel = 1
+	Error     LogLevel = 0
+	InfoColor          = "\033[1;34m%s\033[0m"
 	//NoticeColor  = "\033[1;36m%s\033[0m"
 	//WarningColor = "\033[1;33m%s\033[0m"
-	ErrorColor   = "\033[1;31m%s\033[0m"
-	DebugColor   = "\033[0;36m%s\033[0m"
+	ErrorColor = "\033[1;31m%s\033[0m"
+	DebugColor = "\033[0;36m%s\033[0m"
 )
 
 func DefaultLogOutput(message string, args ...interface{}) {
@@ -34,20 +34,20 @@ func NewLogger(level LogLevel) LogParams {
 func (l LogParams) Info(message string, args ...interface{}) {
 	if l.level >= 1 {
 		computedMessage := fmt.Sprintf(message, args...)
-		fmt.Printf(InfoColor + time.Now().Format(time.RFC3339)+" - Info : %s \n", computedMessage)
+		fmt.Printf(InfoColor + time.Now().Format(time.RFC3339) + " - Info : " + computedMessage + " \n")
 	}
 }
 
 func (l LogParams) Debug(message string, args ...interface{}) {
 	if l.level >= 2 {
 		computedMessage := fmt.Sprintf(message, args...)
-		fmt.Printf(DebugColor + time.Now().Format(time.RFC3339)+" - Debug : %s \n", computedMessage)
+		fmt.Printf(DebugColor + time.Now().Format(time.RFC3339) + " - Debug : %s " + computedMessage + " \n")
 	}
 }
 
 func (l LogParams) Error(message string, args ...interface{}) {
 	if l.level >= 0 {
 		computedMessage := fmt.Sprintf(message, args...)
-		fmt.Printf(ErrorColor + time.Now().Format(time.RFC3339)+" - Error : %s \n", computedMessage)
+		fmt.Printf(ErrorColor + time.Now().Format(time.RFC3339) + " - Error : %s " + computedMessage + " \n")
 	}
 }
