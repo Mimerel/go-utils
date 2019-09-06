23470409cc4b2ad6a67887fd1467605008a708bf
Steven LEGUIN <s.leguin@mimerel.fr>
2019-09-06 14:23:29 +0200

    feat() color change in logs

--- logs.go
+++ logs.go
@@ -16,7 +16,7 @@ const (
 	WarnColor = "\033[1;33m%s\033[0m"
 	ErrorColor = "\033[1;31m%s\033[0m"
 	DebugColor = "\033[0;36m%s\033[0m"
-	DebugPlusColor = "\033[01;33;43m"
+	DebugPlusColor = "\033[1;34m%s\033[0m"
 )
 
 func DefaultLogOutput(message string, args ...interface{}) {
